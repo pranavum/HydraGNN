2a5958d07b553fc4ce118eca3ddfc08ee26682a4
Massimiliano Lupo Pasini <7ml@ornl.gov>
2022-01-24 23:07:31 -0500

    ASE environment used to compute boundary conditions

--- hydragnn/preprocess/utils.py
+++ hydragnn/preprocess/utils.py
@@ -22,13 +22,14 @@ def check_if_graph_size_constant(train_loader, val_loader, test_loader):
     return graph_size_variable
 
 
+import ase
+import torch
 import torch_geometric
-from torch_geometric.transforms import BaseTransform
 from torch_geometric.data import Data
 from torch_geometric.transforms import RadiusGraph, Distance
 
 
-class RadiusGraphPBC(BaseTransform):
+class RadiusGraphPBC(RadiusGraph):
     r"""Creates edges based on node positions :obj:`pos` to all points within a
     given distance.
 
@@ -42,60 +43,82 @@ class RadiusGraphPBC(BaseTransform):
         flow (string, optional): The flow direction when using in combination
             with message passing (:obj:`"source_to_target"` or
             :obj:`"target_to_source"`). (default: :obj:`"source_to_target"`)
+        periodic_boundary_conditions (bool, optional): If :obj:`True`,
+            periodic boundary conditions will be applied. (default: :obj:`False`)
     """
-    def __init__(self, r, loop=False, max_num_neighbors=32, flow='source_to_target', periodic_boundary_conditions=False):
-        self.r = r
-        self.loop = loop
-        self.max_num_neighbors = max_num_neighbors
-        self.flow = flow
+
+    def __init__(
+        self,
+        r,
+        loop=False,
+        max_num_neighbors=32,
+        flow="source_to_target",
+        periodic_boundary_conditions=False,
+    ):
+        super.__init__(r, loop, max_num_neighbors, flow)
         self.periodic_boundary_conditions = periodic_boundary_conditions
 
     def __call__(self, data):
         data.edge_attr = None
-        batch = data.batch if 'batch' in data else None
-        if periodic_boundary_conditions:
-            assert(hasattr(data, 'unit_cell'), "The data must contain information about the size of the unit cell to apply the periodic boundary conditions")
-            assert(hasattr(data, 'atom_types'), "The data must contain information about the atoms at each location of the lattice. Can be a chemical symbol (str) or an atomic number (int).")
-            ase_atom_object = ase.Atoms(symbols=data.atom_types, positions=data.pos, cell=data.unit_cell, pbc=True)
-            edge_src, edge_dst, edge_shift = ase.neighborlist.neighbor_list("ijS", a=ase_atom_object, cutoff=self.r, self_interaction=self.loop)
-
-            #create masks to remove replications of the same edge
-            edge_src_mask = numpy.full(edge_src.shape, True)
-            edge_dst_mask = numpy.full(edge_dst.shape, True)
-            edge_shift_mask = numpy.full(edge_shift.shape, True)
-
-            # the ASE environment enforces periodic boundary conditions so that the same edge may be listed multiple times
-            # we need to remove multiple replicas of the same edge
-            # for each source node in the graph, we create a unique set that lists all the nodes connected to it (including nodes that connet through periodic boundary conditions)
-
-            src_dictionary = {}
-            for index in range(edge_src.shape[0]):
-                if edge_src[index] in src_dictionary:
-                   if edge_dst[index] in src_dictionary[edge_src[index]]:
-                      edge_src_mask[index] = False
-                      edge_dst_mask[index] = False
-                      edge_shift_mask[index][0] = False
-                      edge_shift_mask[index][1] = False
-                      edge_shift_mask[index][2] = False
-                   else:
-                      src_dictionary[edge_src[index]].add(edge_dst[index])
-                else:
-                   src_dictionary[edge_src[index]] = set()
-                   src_dictionary[edge_src[index]].add(edge_dst[index])
-
-            edge_src = edge_src[edge_src_mask]
-            edge_dst = edge_dst[edge_dst_mask]
-            edge_shift = edge_shift[edge_shift_mask]
-            edge_shift = edge_shift.reshape(-1,3)
-
-            data.edge_index=torch.stack([torch.LongTensor(edge_src), torch.LongTensor(edge_dst)], dim=0),
-            data.edge_shift=torch.tensor(edge_shift, dtype=default_dtype),
+        if self.periodic_boundary_conditions:
+            data.pbc = True
+            assert (
+                "batch" not in data
+            ), "periodic boundary conditions not currently supported on batches"
+            assert hasattr(
+                data, "unit_cell"
+            ), "The data must contain information about the size of the unit cell to apply the periodic boundary conditions"
+            assert hasattr(
+                data, "atom_types"
+            ), "The data must contain information about the atoms at each location of the lattice. Can be a chemical symbol (str) or an atomic number (int)."
+            ase_atom_object = ase.Atoms(
+                symbols=data.atom_types,
+                positions=data.pos,
+                cell=data.unit_cell,
+                pbc=True,
+            )
+            edge_src, edge_dst, edge_shift = ase.neighborlist.neighbor_list(
+                "ijS", a=ase_atom_object, cutoff=self.r, self_interaction=self.loop
+            )
+            distance_matrix = ase_atom_object.get_all_distances(mic=True)
+            data.edge_index = torch.stack(
+                [torch.LongTensor(edge_src), torch.LongTensor(edge_dst)], dim=0
+            )
+            data.edge_shift = torch.tensor(edge_shift)
+            data.edge_attr = torch.zeros(
+                ase_atom_object.get_global_number_of_atoms(), 1
+            )
+            for index in range(0, edge_src.shape[0]):
+                data.edge_attr[index, 1] = distance_matrix[
+                    edge_src[index], edge_dst[index]
+                ]
         else:
-            data.edge_index = torch_geometric.nn.radius_graph(
-                data.pos, self.r, batch, self.loop, self.max_num_neighbors,
-                self.flow)
+            data = super().__call__(data)
+
         return data
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(r={self.r})'
+        return f"{self.__class__.__name__}(r={self.r})"
 
+
+class DistancePBC(Distance):
+    r"""Saves the Euclidean distance of linked nodes in its edge attributes.
+
+    Args:
+        norm (bool, optional): If set to :obj:`False`, the output will not be
+            normalized to the interval :math:`[0, 1]`. (default: :obj:`True`)
+        max_value (float, optional): If set and :obj:`norm=True`, normalization
+            will be performed based on this value instead of the maximum value
+            found in the data. (default: :obj:`None`)
+        cat (bool, optional): If set to :obj:`False`, all existing edge
+            attributes will be replaced. (default: :obj:`True`)
+    """
+
+    def __call__(self, data):
+        if (not hasattr(data, "pbc")) or (not data.pbc):
+            super.__call__(data)
+        else:
+            # If the connectivity of the data uses boundary conditions, then the edge length has already been added as an attribute
+            assert hasattr(data, "attr")
+
+        return data
