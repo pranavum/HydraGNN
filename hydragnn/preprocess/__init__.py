7f34e9e1d88c3c216f900fd0bad431d0a25a28aa
Sam Reeve <6740307+streeve@users.noreply.github.com>
2022-02-03 12:30:31 -0500

    Separate edges/lengths with loader; PBC class does not revert to base; move config convenience functions to utils

--- hydragnn/preprocess/__init__.py
+++ hydragnn/preprocess/__init__.py
@@ -1,5 +1,10 @@
 from .dataset_descriptors import AtomFeatures, StructureFeatures
-from .utils import check_if_graph_size_constant, RadiusGraphPBC, DistancePBC
+from .utils import (
+    check_if_graph_size_constant,
+    get_radius_graph_config,
+    get_radius_graph_pbc_config,
+    RadiusGraphPBC,
+)
 from .load_data import (
     dataset_loading_and_splitting,
     create_dataloaders,
@@ -10,7 +15,6 @@ from .load_data import (
 from .serialized_dataset_loader import (
     SerializedDataLoader,
     update_predicted_values,
-    get_radius_graph,
 )
 from .raw_dataset_loader import RawDataLoader
 from .compositional_data_splitting import compositional_stratified_splitting
