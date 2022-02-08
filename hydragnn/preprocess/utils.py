8586fdd6e598d1c7e24c38b9d71696b847bb3909
Massimiliano Lupo Pasini <7ml@ornl.gov>
2022-02-08 11:54:39 -0500

    unit test for periodic boundary condition fixed. Caveat: the same edge is replicated multiple times

--- hydragnn/preprocess/utils.py
+++ hydragnn/preprocess/utils.py
@@ -77,7 +77,7 @@ class RadiusGraphPBC(RadiusGraph):
         data.edge_index = torch.stack(
             [torch.LongTensor(edge_src), torch.LongTensor(edge_dst)], dim=0
         )
-        data.edge_attr = torch.zeros(ase_atom_object.get_global_number_of_atoms(), 1)
+        data.edge_attr = torch.zeros(edge_src.shape[0], 1)
         for index in range(0, edge_src.shape[0]):
             data.edge_attr[index, 1] = distance_matrix[edge_src[index], edge_dst[index]]
 
